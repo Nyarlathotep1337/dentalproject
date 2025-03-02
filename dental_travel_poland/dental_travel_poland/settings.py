0899ab5fb3ce8aa2043c9780adbe1903e7933a74
Jakub Rogalski <jakubrogalski@protonmail.com>
2025-03-02 09:45:43 +0100

    Wykonano migracje i podłączono bazę danych PostgreSQL

--- dental_travel_poland/dental_travel_poland/settings.py
+++ dental_travel_poland/dental_travel_poland/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'dental_travel_poland.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dental_travel',
+        'USER': 'postgres',  # Upewnij się, że to twoja nazwa użytkownika PostgreSQL
+        'PASSWORD': 'Ebola',  # Wpisz hasło, które ustawiłeś
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
